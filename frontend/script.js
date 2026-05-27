c7795de0bbb900cb98978785d911841d21b75d76
sebagalva <seba.galva@gmail.com>
2026-05-27 19:48:22 +0200

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
 async function loadWB() {
-  const res = await fetch("https://TUO_BACKEND.up.railway.app/lastWB");
+  const res = await fetch("wb-timer-production.up.railway.app/lastWB");
   const data = await res.json();
   document.getElementById("value").innerText = data.lastWB;
 }
