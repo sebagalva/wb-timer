f030d1a522dd6adbdf6278a2ce1f895b36c23fd4
sebagalva <seba.galva@gmail.com>
2026-05-27 19:42:25 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-async function loadWB() {
-  const res = await fetch("https://TUO_BACKEND.up.railway.app/lastWB");
-  const data = await res.json();
-  document.getElementById("value").innerText = data.lastWB;
-}
-
-loadWB();
-setInterval(loadWB, 10000);
